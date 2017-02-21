3b441bf5b1c2008d5450c4a0bbe432127d98fcd4
Susan Barrows <sxbarrows@gmail.com>
2017-02-21 18:29:46 +0000

    added contact create action to save db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,30 @@
 class ContactsController < ApplicationController
-    def new
+  
+  # GET request to /contact-us
+  # Show new contact form
+  def new
+    @contact = Contact.new
+  end
+  
+  # POST request /contacts
+  def create
+    # Mass assignment of form fields into Contact object
+    @contact = Contact.new(contact_params)
+    # Save the Contact object to the database
+    if @contact.save
+      redirect_to new_contact_path
+    else
+      # If Contact object doesn't save,
+      # store errors in flash hash,
+      # and redirect to the new action
+      redirect_to new_contact_path
     end
-end    
+  end
+  
+  private
+    # To collect data from form, we need to use
+    # strong parameters and whitelist the form fields
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
+end

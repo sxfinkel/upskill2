a2f1f1642475cbbd1235b74b7f6fd307af413896
Susan Barrows <sxbarrows@gmail.com>
2017-02-22 04:16:43 +0000

    got message sent and message warning working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,20 +3,21 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   
-  def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      name = params[:contact][:name]
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
-      ContactMailer.contact_email(name, email, body).deliver
-      flash[:success] = "Message sent."
-      redirect_to new_contact_path
-    else
-      flash[:danger] = @contact.errors.full_messages.join(", ")
-      redirect_to new_contact_path
-    end
+ def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    
+     name = params[:contact][:name]
+     email = params[:contact][:email]
+     body = params[:contact][:comments]
+     ContactMailer.contact_email(name, email, body).deliver
+     flash[:success] = "Message sent."
+     redirect_to new_contact_path
+  else
+     flash[:danger] = @contact.errors.full_messages.join(", ")
+     redirect_to new_contact_path
   end
+ end
   
   private
     def contact_params

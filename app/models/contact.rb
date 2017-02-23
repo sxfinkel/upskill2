9688c6c7507b3be552feb0da5c3834012b934a8f
Susan Barrows <sxbarrows@gmail.com>
2017-02-23 03:40:57 +0000

    added contact model file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,2 @@
 class Contact < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :comments, presence: true
-    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221174937) do
+ActiveRecord::Schema.define(version: 20170223033502) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"

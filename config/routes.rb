bf7ac20dad09cddcf9223cf249287c01e7c1ad05
Susan Barrows <sxbarrows@gmail.com>
2017-02-23 03:22:09 +0000

    Added contact form view, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  resources :contacts, only: :create
-  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+ root to:'pages#home'
+ get 'about', to: 'pages#about'
+ resources :contacts
 end

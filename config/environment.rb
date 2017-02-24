5e2117541ef825044a869f4bbc6149144b26fb9c
Susan Barrows <sxbarrows@gmail.com>
2017-02-24 17:57:46 +0000

    added mailer settings to env file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'powerful-plains-21724.herokuapp.com',
+  :domain         => 'ijazz.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
